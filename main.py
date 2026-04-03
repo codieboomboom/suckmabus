f0f01bb5835d1b80b20cbbe3bbeabcb7e2ffee96
Anh Tu Do <anhtu.do1998@gmail.com>
2026-04-03 21:45:55 +0800

    Added local db loading and saving, plus functionality to fetch all bus stops

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import requests
 from dotenv import load_dotenv
 from os import environ
+from pathlib import Path
+import json
 
 load_dotenv()
 TELEGRAM_BOT_TOKEN=environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_BOT_BASEURL=f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
+LTA_TOKEN=environ.get("LTA_DATAMALL_TOKEN")
+LTA_BASEURL="https://datamall2.mytransport.sg/ltaodataservice"
 
+DB_FILE = "database.json"
+
+BOT_STATE_IDLE = "idle"
+BOT_STATE_REGISTER_PENDING_BUS_STOP_NUM = "pending_bus_stop_num"
+BOT_STATE_REGISTER_PENDING_BUS_STOP_ALIAS = "pending_bus_stop_alias"
+
+# Global startup
+curr_state = BOT_STATE_IDLE
+    
+# Data store helper=========
+def load_db():
+    with open(DB_FILE, "r", encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+def save_db(data):
+    with open(DB_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+# Telegram stuffs ===========
 def get_updates(offset=None):
     """Get updates from user"""
     params = {
@@ -83,10 +107,40 @@ def handle_update(update):
     elif command == "/check":
         handle_check()
 
-    else:
-        send_message(chat_id, f"I don't understand you. Please try /help")
+        else:
+            send_message(chat_id, f"I don't understand you. Please try /help")
+
+def fetch_all_bus_stops():
+    url = f"{LTA_BASEURL}/BusStops"
+    headers = {
+        "accountKey": LTA_TOKEN
+    }
+    resp = requests.get(url, headers=headers)
+
+    json_result = resp.json()
+
+    if "value" not in json_result or not json_result["value"]:
+        return {}
+
+    bus_stop_lists = json_result["value"]
+    bus_stops_dict = {}
+
+    for bus_stop_entry in bus_stop_lists:
+        bus_stops_dict[bus_stop_entry["BusStopCode"]] = bus_stop_entry
+
+    return bus_stops_dict
+
+
 
 def run():
+    if not Path(DB_FILE).exists():
+        save_db(data={})
+
+    db = load_db()
+    if "bus_stops" not in db:
+        db["bus_stops"] = fetch_all_bus_stops()
+    save_db(db)
+
     offset = None
     print("Bot is running...")
 
@@ -98,9 +152,7 @@ def run():
         for update in updates:
             print(update)
             handle_update(update)
-            offset = update["update_id"] + 1
-        
-        
+            offset = update["update_id"] + 1        
 
 if __name__=="__main__":
     run()
