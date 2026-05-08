b6322a91877d79823c52a15e56d2ec25ee911f70
Anh Tu Do <anhtu.do1998@gmail.com>
2026-05-08 19:31:36 +0800

    Reformat

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from enum import Enum, auto
 import json
 
 load_dotenv()
-TELEGRAM_BOT_TOKEN=environ.get("TELEGRAM_BOT_TOKEN")
-TELEGRAM_BOT_BASEURL=f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
-LTA_TOKEN=environ.get("LTA_DATAMALL_TOKEN")
-LTA_BASEURL="https://datamall2.mytransport.sg/ltaodataservice"
+TELEGRAM_BOT_TOKEN = environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_BASEURL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
+LTA_TOKEN = environ.get("LTA_DATAMALL_TOKEN")
+LTA_BASEURL = "https://datamall2.mytransport.sg/ltaodataservice"
 
-LTA_API_SKIP_OFFSET = 500 # max number of record fetched per call to API
+LTA_API_SKIP_OFFSET = 500  # max number of record fetched per call to API
 # ============ States ===========================
+
 
 class State(Enum):
     IDLE = auto()
@@ -22,27 +23,27 @@ class State(Enum):
     DEREGISTER_AWAITING_CONFIRM = auto()
     CHECK_AWAITING_SELECTION = auto()
 
+
 # ============== Sessions Management =====================
-sessions : dict[int] = {}
+sessions: dict[int] = {}
+
 
 def get_session(chat_id: int) -> dict:
     # Get session-specific data for the user identified
     # by chat_id
     if chat_id not in sessions:
-        sessions[chat_id] = {
-            "state": State.IDLE,
-            "data": {}
-        }
-        print(f"[get_session] Create a new session entry for chat_id {chat_id} as not existed previously")
+        sessions[chat_id] = {"state": State.IDLE, "data": {}}
+        print(
+            f"[get_session] Create a new session entry for chat_id {chat_id} as not existed previously"
+        )
     return sessions[chat_id]
+
 
 def reset_session(chat_id: int):
     # Reset the chat_id session state back to IDLE
-    sessions[chat_id] = {
-        "state": State.IDLE,
-        "data": {}
-    }
+    sessions[chat_id] = {"state": State.IDLE, "data": {}}
     print(f"[reset_session] Session for chat_id {chat_id} has been reset!")
+
 
 def update_state_and_data(chat_id: int, next_state: State, **updates):
     # Helper to update state and data to chat_id sessions
@@ -52,27 +53,27 @@ def update_state_and_data(chat_id: int, next_state: State, **updates):
     print(f"[update_state_and_data] Updated to State: {next_state} and Data: {updates}")
     print(f"[update_state_and_data] Sessions view {sessions}")
 
+
 # ============= DB Management ===========================
 DATABASE_URL = "database.json"
 
+
 def write_to_db(data: dict):
-    with open(DATABASE_URL, "w", encoding='utf-8') as db:
+    with open(DATABASE_URL, "w", encoding="utf-8") as db:
         json.dump(data, db, indent=4)
 
+
 def read_from_db():
-    with open(DATABASE_URL, "r", encoding='utf-8') as db:
+    with open(DATABASE_URL, "r", encoding="utf-8") as db:
         data = json.load(db)
     return data
+
 
 def init_db():
     if not Path(DATABASE_URL).exists():
         bus_stops_data = fetch_all_bus_stops()
-        write_to_db(
-            data = {
-                "bus_stops": bus_stops_data,
-                "registered": {}
-            }
-        )
+        write_to_db(data={"bus_stops": bus_stops_data, "registered": {}})
+
 
 # Telegram stuffs ===========
 def get_updates(offset=None):
@@ -80,47 +81,45 @@ def get_updates(offset=None):
     # Long polling
     # only interested in "message" or "callback_query"
     params = {
-        "timeout": 30, # long polling
-        "allowed_updates": ["message", "callback_query"]
+        "timeout": 30,  # long polling
+        "allowed_updates": ["message", "callback_query"],
     }
     if offset:
         params["offset"] = offset
 
     resp = requests.get(
-        url=f"{TELEGRAM_BOT_BASEURL}/getUpdates", 
+        url=f"{TELEGRAM_BOT_BASEURL}/getUpdates",
         params=params,
-        timeout=35 # need to be longer than the timeout from telegram bot, otw will cut it off prematurely
+        timeout=35,  # need to be longer than the timeout from telegram bot, otw will cut it off prematurely
     )
     return resp.json()
 
 
 def send_message(chat_id, text, **kwargs):
     """Send a reply back to user"""
-    msg = {
-        "chat_id": chat_id,
-        "text": text
-    }
+    msg = {"chat_id": chat_id, "text": text}
     if kwargs:
         msg.update(kwargs)
     requests.post(url=f"{TELEGRAM_BOT_BASEURL}/sendMessage", json=msg)
+
 
 def parse_command(text: str):
     # Parse and return the name of command and arguments
     # if text is not command, return None and an empty argument list
     if not text.startswith("/") and len(text) > 1:
         return (None, [])
-    
+
     command_and_args = text[1:].split()
 
     return (command_and_args[0], command_and_args[1:])
 
+
 def answer_callback_query(callback_query_id, **kwargs):
-    payload = {
-        "callback_query_id": callback_query_id         
-    }
+    payload = {"callback_query_id": callback_query_id}
     if kwargs:
         payload.update(kwargs)
     requests.post(url=f"{TELEGRAM_BOT_BASEURL}/answerCallbackQuery", json=payload)
+
 
 def handle_callback_query(callback_query):
     print("[handle_callback_query")
@@ -128,9 +127,11 @@ def handle_callback_query(callback_query):
     callback_query_id = callback_query["id"]
     if not callback_query_id:
         raise ValueError("callback_query_id must be present inside a callback query")
-    
+
     answer_callback_query(callback_query_id)
-    print(f"[handle_callback_query] Answered to callback_query with id {callback_query_id}")
+    print(
+        f"[handle_callback_query] Answered to callback_query with id {callback_query_id}"
+    )
 
     # Process the callback query from button clicks
     callback_data = callback_query["data"]
@@ -138,7 +139,9 @@ def handle_callback_query(callback_query):
         return
 
     command_in_process, tbc_resource_type, chat_id, verdict = callback_data.split(":")
-    print(f"[handle_callback_query] Callback data parsing: {command_in_process} {tbc_resource_type} {chat_id} {verdict}")
+    print(
+        f"[handle_callback_query] Callback data parsing: {command_in_process} {tbc_resource_type} {chat_id} {verdict}"
+    )
 
     # TODO: Does Telegram API works with integer chat_id only? Here need to
     # convert back to integer as the way button callback make it a text
@@ -148,7 +151,7 @@ def handle_callback_query(callback_query):
     # Function the FSM
     curr_session = get_session(chat_id)
     curr_state = curr_session["state"]
-    curr_session_data = curr_session["data"] 
+    curr_session_data = curr_session["data"]
 
     if curr_state == State.REGISTER_AWAITING_CONFIRM and command_in_process == "reg":
         if tbc_resource_type == "bus_stop" and verdict == "yes":
@@ -159,7 +162,7 @@ def handle_callback_query(callback_query):
             bus_stop_entry = {
                 "bus_stop_num": pending_bus_stop_num,
                 "road_name": db_data["bus_stops"][pending_bus_stop_num]["RoadName"],
-                "desc": db_data["bus_stops"][pending_bus_stop_num]["Description"]
+                "desc": db_data["bus_stops"][pending_bus_stop_num]["Description"],
             }
             if "registered" not in db_data:
                 db_data["registered"] = {}
@@ -176,14 +179,15 @@ def handle_callback_query(callback_query):
             reset_session(chat_id)
         elif tbc_resource_type == "bus_stop" and verdict == "no":
             print("[handle_callback_query][confirm bus stop no]")
-            text = f"Please provide another bus stop number or /cancel"
+            text = "Please provide another bus stop number or /cancel"
             send_message(chat_id, text)
             update_state_and_data(chat_id, State.REGISTER_AWAITING_BUS_STOP_NUM)
+
 
 def handle_message(message):
     print("[handle_message]")
     chat_id = message["chat"]["id"]
-    text = message.get("text", "")   # could be a sticker/photo with no text
+    text = message.get("text", "")  # could be a sticker/photo with no text
 
     if not chat_id:
         raise ValueError("chat_id must be present in message type")
@@ -192,11 +196,13 @@ def handle_message(message):
 
     if command == "help":
         # Display help message
-        send_message(chat_id, "Hi, I can help you with: \n"
+        send_message(
+            chat_id,
+            "Hi, I can help you with: \n"
             "/reg - Register a bus stop\n"
             "/dereg - Remove a bus stop\n"
             "/modify - Edit bus stop information\n"
-            "/check - Check arrival timings"
+            "/check - Check arrival timings",
         )
         return
     elif command == "start":
@@ -255,20 +261,34 @@ on {bus_stop_road_name}
                 # question.
                 confirm_keyboard_markup = {
                     "inline_keyboard": [
-                        [{"text": "Yes", "callback_data": f"reg:bus_stop:{chat_id}:yes"},
-                         {"text": "No", "callback_data": f"reg:bus_stop:{chat_id}:no"}]
+                        [
+                            {
+                                "text": "Yes",
+                                "callback_data": f"reg:bus_stop:{chat_id}:yes",
+                            },
+                            {
+                                "text": "No",
+                                "callback_data": f"reg:bus_stop:{chat_id}:no",
+                            },
+                        ]
                     ]
                 }
                 kwargs = {"reply_markup": confirm_keyboard_markup}
                 send_message(chat_id, reply_msg, **kwargs)
                 # Store temporarily the bus_stop_num and move to wait for user confirm
-                ss_data_to_store  = {"bus_stop_num": bus_stop_num}
-                update_state_and_data(chat_id, next_state=State.REGISTER_AWAITING_CONFIRM, **ss_data_to_store)
-            
+                ss_data_to_store = {"bus_stop_num": bus_stop_num}
+                update_state_and_data(
+                    chat_id,
+                    next_state=State.REGISTER_AWAITING_CONFIRM,
+                    **ss_data_to_store,
+                )
+
     else:
         reset_session(chat_id)
-        send_message(chat_id, "🥺 Sorry I don't understand you! Please refer to /help and try again!")
-
+        send_message(
+            chat_id,
+            "🥺 Sorry I don't understand you! Please refer to /help and try again!",
+        )
 
 
 def handle_update(update):
@@ -290,7 +310,10 @@ def handle_update(update):
         handle_message(msg)
     else:
         # an unimplemented update
-        print("[handle_update] Received an update that is not callback_query nor message")
+        print(
+            "[handle_update] Received an update that is not callback_query nor message"
+        )
+
 
 def fetch_all_bus_stops():
     """
@@ -298,11 +321,9 @@ def fetch_all_bus_stops():
     where key is the bus stop number and the value is a dictionary of key-value
     pairs representing information of the bus stops
     """
-    headers = {
-        "accountKey": LTA_TOKEN
-    }
+    headers = {"accountKey": LTA_TOKEN}
     bus_stops = []
-    skip_by = 0 # Number of entry to skip
+    skip_by = 0  # Number of entry to skip
     # Based on https://datamall.lta.gov.sg/content/dam/datamall/datasets/LTA_DataMall_API_User_Guide.pdf
     # Currently, each API call only returns max LTA_API_SKIP_OFFSET
     # Here we needs to handle them until all entries are fetched
@@ -318,7 +339,7 @@ def fetch_all_bus_stops():
         if len(json_result["value"]) < LTA_API_SKIP_OFFSET:
             break
 
-        skip_by += LTA_API_SKIP_OFFSET 
+        skip_by += LTA_API_SKIP_OFFSET
 
     # Need to be key-value format for easier storage into file database
     bus_stops_dict = {}
@@ -339,7 +360,8 @@ def run():
 
         for update in updates:
             handle_update(update)
-            offset = update["update_id"] + 1        
+            offset = update["update_id"] + 1
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     run()
